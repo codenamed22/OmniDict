c4a0c7d5a2edf09057457ea5dded712730dedeae
chilli-garlic-momo <jaiswaljiya001@gmail.com>
2025-06-22 18:08:50 +0530

    Added commands to root file + Binary file creation

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,28 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "omnidict",
-	Short: "OmniDict CLI – Distributed KV Store",
+	Short: "OmniDict CLI - Distributed KV Store",
 }
 
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	// Add all subcommands here
+	rootCmd.AddCommand(deleteCmd)
+	rootCmd.AddCommand(existsCmd)
+	rootCmd.AddCommand(expireCmd)
+	rootCmd.AddCommand(flushCmd)
+	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(keysCmd)
+	rootCmd.AddCommand(putCmd)
+	rootCmd.AddCommand(ttlCmd)
+	rootCmd.AddCommand(updateCmd)
 }
