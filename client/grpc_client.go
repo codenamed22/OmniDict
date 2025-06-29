19c7a4be4c4f206123100b135832dfd38071b0df
chilli-garlic-momo <jaiswaljiya001@gmail.com>
2025-06-29 05:43:23 +0530

    Added Cluster Transaction for the client side

--- client/grpc_client.go
+++ client/grpc_client.go
@@ -4,74 +4,137 @@ import (
 	"context"
 	"log"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	pb_kv "omnidict/proto/kv"
+
+	"google.golang.org/grpc"
 )
 
-var Conn *grpc.ClientConn
+var (
+	globalClient *Client
+)
 
-func InitGRPCClient(endpoint string) {
-	var err error
-	Conn, err = grpc.Dial(
-		endpoint,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
+type Transaction struct {
+	ID     string
+	Client pb_kv.OmnidictServiceClient
+	Ops    []*pb_kv.TxnOperation
+}
+
+type Client struct {
+	conn     *grpc.ClientConn
+	kvClient pb_kv.OmnidictServiceClient
+}
+
+func InitGRPCClient(addr string) {
+	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("Failed to connect: %v", err)
+		log.Fatalf("did not connect: %v", err)
+	}
+	globalClient = &Client{
+		conn:     conn,
+		kvClient: pb_kv.NewOmnidictServiceClient(conn),
+	}
+}
+
+func Close() {
+	if globalClient != nil && globalClient.conn != nil {
+		globalClient.conn.Close()
 	}
 }
 
 func Put(key string, value []byte) (*pb_kv.PutResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn) // Fixed service name
-	return client.Put(context.Background(), &pb_kv.PutRequest{
+	return globalClient.kvClient.Put(context.Background(), &pb_kv.PutRequest{
 		Key:   key,
 		Value: string(value),
 	})
 }
 
 func Get(key string) (*pb_kv.GetResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Get(context.Background(), &pb_kv.GetRequest{Key: key})
+	return globalClient.kvClient.Get(context.Background(), &pb_kv.GetRequest{Key: key})
 }
 
 func Delete(key string) (*pb_kv.DeleteResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Delete(context.Background(), &pb_kv.DeleteRequest{Key: key})
+	return globalClient.kvClient.Delete(context.Background(), &pb_kv.DeleteRequest{Key: key})
 }
 
 func Exists(key string) (*pb_kv.ExistsResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Exists(context.Background(), &pb_kv.ExistsRequest{Key: key})
+	return globalClient.kvClient.Exists(context.Background(), &pb_kv.ExistsRequest{Key: key})
 }
 
 func Expire(key string, ttl int64) (*pb_kv.ExpireResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Expire(context.Background(), &pb_kv.ExpireRequest{
+	return globalClient.kvClient.Expire(context.Background(), &pb_kv.ExpireRequest{
 		Key: key,
 		Ttl: ttl,
 	})
 }
 
 func Flush() (*pb_kv.FlushResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Flush(context.Background(), &pb_kv.FlushRequest{})
+	return globalClient.kvClient.Flush(context.Background(), &pb_kv.FlushRequest{})
 }
 
 func Keys(pattern string) (*pb_kv.KeysResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Keys(context.Background(), &pb_kv.KeysRequest{Pattern: pattern})
+	return globalClient.kvClient.Keys(context.Background(), &pb_kv.KeysRequest{Pattern: pattern})
 }
 
 func TTL(key string) (*pb_kv.TTLResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.TTL(context.Background(), &pb_kv.TTLRequest{Key: key})
+	return globalClient.kvClient.TTL(context.Background(), &pb_kv.TTLRequest{Key: key})
 }
 
 func Update(key string, value []byte) (*pb_kv.UpdateResponse, error) {
-	client := pb_kv.NewOmnidictServiceClient(Conn)
-	return client.Update(context.Background(), &pb_kv.UpdateRequest{
+	return globalClient.kvClient.Update(context.Background(), &pb_kv.UpdateRequest{
 		Key:   key,
 		Value: string(value),
 	})
+}
+
+func BeginTransaction() (*Transaction, error) {
+	resp, err := globalClient.kvClient.BeginTransaction(context.Background(), &pb_kv.BeginTxnRequest{})
+	if err != nil {
+		return nil, err
+	}
+	return &Transaction{
+		ID:     resp.TxnId,
+		Client: globalClient.kvClient,
+	}, nil
+}
+
+func (t *Transaction) Put(key, value string) {
+	t.Ops = append(t.Ops, &pb_kv.TxnOperation{
+		Key:   key,
+		Value: value,
+		Op:    pb_kv.TxnOperation_SET,
+	})
+}
+
+func (t *Transaction) Delete(key string) {
+	t.Ops = append(t.Ops, &pb_kv.TxnOperation{
+		Key: key,
+		Op:  pb_kv.TxnOperation_DELETE,
+	})
+}
+
+func (t *Transaction) Commit() (bool, error) {
+	// Prepare phase
+	prepareResp, err := t.Client.Prepare(context.Background(), &pb_kv.PrepareRequest{
+		TxnId:      t.ID,
+		Operations: t.Ops,
+	})
+	if err != nil || !prepareResp.Success {
+		t.Client.Abort(context.Background(), &pb_kv.AbortRequest{TxnId: t.ID})
+		return false, err
+	}
+
+	// Commit phase
+	commitResp, err := t.Client.Commit(context.Background(), &pb_kv.CommitRequest{TxnId: t.ID})
+	if err != nil {
+		return false, err
+	}
+	return commitResp.Success, nil
+}
+
+func (t *Transaction) Abort() (bool, error) {
+	abortResp, err := t.Client.Abort(context.Background(), &pb_kv.AbortRequest{TxnId: t.ID})
+	if err != nil {
+		return false, err
+	}
+	return abortResp.Success, nil
 }
